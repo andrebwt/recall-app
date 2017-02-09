bf2121ec64393c5986a07d2f7fc42b4691f0de7f
Andre Braithwaite <andrebwt@gmail.com>
2017-02-09 22:50:36 +0000

    Testing external js functions work.

---
+++ js/buildTable.js
@@ -0,0 +1,17 @@
+/**
+ * Created by andre on 09/02/2017.
+ */
+
+
+
+function loadXMLDoc() {
+
+
+
+}
+
+function sum(x, y) {
+    return x + y;
+}
+
+document.getElementById("showCard").innerHTML = sum(2,3)
